2cbc66b05cd7cc91616ddbc28c3e3f3c90807a91
sugan84 <sugandran@gmail.com>
2021-10-06 10:34:45 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+	var principal = document.getElementById("principal").value;
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+	var interest = principal * years * rate /100;
+	var year = new Date().getFullYear() + parseInt(years);
     
+	//Create the Interest text
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + year + "</mark>";
 }
-        
+    
+
+function getSliderValue() {
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
+}
+    
+	
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+} 
